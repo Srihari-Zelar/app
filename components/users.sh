adfd49bf81bd5a2771c3096f666aeb1202fa392c
Srihari <Srihari@zelarsoft.com>
2021-06-16 15:27:11 +0530

    update

--- components/users.sh
+++ components/users.sh
@@ -19,21 +19,27 @@ Head "installing java"
 apt install openjdk-8-jre-headless -y &>>$LOG
 
 apt install openjdk-8-jdk-headless -y &>>$LOG
+Stat $?
 
 Head "exporting to java-home"
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 &>>$LOG
+Stat $?
 
 Head "installing maven"
 apt install maven -y &>>$LOG
+Stat $?
 
 cd /home/app/users/
+Stat $?
 
 Head "installing maven packages"
 mvn clean package &>>$LOG
+Stat $?
 
 cd target/
 
 java -jar users-api-0.0.1.jar
+Stat $?
 
 Head "Setup the systemd Service"
 mv systemd.service /etc/systemd/system/users.service &>>$LOG
