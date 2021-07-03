8e726ec3a61fb3a10741608ba2aba2abc27092c5
Srihari <Srihari@zelarsoft.com>
2021-07-03 13:32:02 +0530

    update

--- components/frontend.sh
+++ components/frontend.sh
@@ -37,7 +37,7 @@ cd frontend
 Head "run and build npm"
 
 npm install -g npm@latest &>>$LOG
-#npm install shelljs &>>$LOG
+npm install shelljs &>>$LOG
 #npm install --save-dev  --unsafe-perm node-sass &>>$LOG
 npm run build  &>>$LOG
 Stat $?
